59d82a757ba612eff80770e91b7ae38c3052a63a
kawanishik <kk.yuzio19@gmail.com>
2022-05-28 18:40:25 +0900

    文字列復元のテストファイルの更新

--- include/dynamic_replacement/test_restore_string.cpp
+++ include/dynamic_replacement/test_restore_string.cpp
@@ -39,7 +39,9 @@ int main() {
     map_dr<plain_bonsai_trie_dr<>, plain_bonsai_nlm_dr<int>> new_map(8);
     uint64_t node_id = 1;   // 比較対象のノード番号
     std::string store_string = "";
-    map.test_CPD_order_and_insert_new_dic(new_map, children, node_id, cnt_leaf_per_node, 0, store_string);
+    std::vector<std::string> restore_keys_order;                                // 復元した文字列順を保存
+    std::vector<std::pair<std::string, uint64_t>> not_leaf_node_compare_string; // 葉ノード以外の文字列を復元する際に比較した文字列とprefixの長さ
+    map.test_CPD_order_and_insert_new_dic(new_map, children, node_id, cnt_leaf_per_node, 0, store_string, restore_keys_order, not_leaf_node_compare_string);
     
     bool test_check = true;
     for(int i=0; i < keys.size(); i++) {
@@ -53,7 +55,7 @@ int main() {
 
     std::cout << "serch_ok." << std::endl;
 
-    std::vector<std::string> keys_exp = {
+    std::vector<std::string> restore_keys_order_exp = {
         "aaaaaaccc",
         "aaaaaabbb",
         "aaabbbccc",
@@ -64,6 +66,32 @@ int main() {
         "aaaaaaaaa"
     };
 
-    std::cout << "比較するキー" << std::endl;
-    for(auto key : keys_exp) std::cout << key << std::endl;
+    std::vector<std::pair<std::string, uint64_t>> not_leaf_node_compare_string_exp {
+        {"aaabbbbbb", 4},
+        {"bbbcccccc", 4},
+        {"bbbbbbbbb", 1}
+    };
+
+    // 文字列の復元順のテスト
+    bool check_restore_key_order = true;
+    if(restore_keys_order.size() != restore_keys_order_exp.size()) check_restore_key_order = false;
+    if(check_restore_key_order) {
+        for(int i=0; i < restore_keys_order_exp.size(); i++) {
+            if(restore_keys_order[i] != restore_keys_order_exp[i]) check_restore_key_order = false;
+        }
+    }
+    
+    // prefixを使用して復元した文字列、抽出に使用した文字列と長さのテスト
+    bool check_not_leaf_node_compare_string = true;
+    if(not_leaf_node_compare_string.size() != not_leaf_node_compare_string_exp.size()) check_not_leaf_node_compare_string = false;
+    if(check_not_leaf_node_compare_string) {
+        for(int i=0; i < not_leaf_node_compare_string_exp.size(); i++) {
+            if(not_leaf_node_compare_string[i].first != not_leaf_node_compare_string_exp[i].first) check_not_leaf_node_compare_string = false;
+            if(not_leaf_node_compare_string[i].second != not_leaf_node_compare_string_exp[i].second) check_not_leaf_node_compare_string = false;
+        }
+    }
+
+    // テスト結果の出力
+    std::cout << "文字列の復元順のテスト : " << (check_restore_key_order ? "ok." : "failed.") << std::endl;
+    std::cout << "部分文字列のテスト     : " << (check_not_leaf_node_compare_string ? "ok." : "failed.") << std::endl;
 }
