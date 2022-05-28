fe05e25700181c4d7370b290f76a0ad3333b437c
kawanishik <kk.yuzio19@gmail.com>
2022-05-28 16:13:54 +0900

    CPD順と文字列復元のためのテストファイルの修正

--- include/dynamic_replacement/test_restore_string.cpp
+++ include/dynamic_replacement/test_restore_string.cpp
@@ -1,5 +1,6 @@
 /*
 文字列を復元し、新しい辞書に追加する際に使用するテストファイル
+確かめる用の関数を使用して、動作を確認している
 */
 
 #include "map_dr.hpp"
@@ -37,8 +38,32 @@ int main() {
 
     map_dr<plain_bonsai_trie_dr<>, plain_bonsai_nlm_dr<int>> new_map(8);
     uint64_t node_id = 1;   // 比較対象のノード番号
-    map.require_centroid_path_order_and_insert_dictionary_using_backward_cumulative(new_map, children, node_id, cnt_leaf_per_node, 0);
+    std::string store_string = "";
+    map.test_CPD_order_and_insert_new_dic(new_map, children, node_id, cnt_leaf_per_node, 0, store_string);
     
+    bool test_check = true;
+    for(int i=0; i < keys.size(); i++) {
+        const int *ptr = map.find(keys[i]);
+        if(not (ptr != nullptr and *ptr == 1)) {
+            std::cout << "search_failed : " << i << "," << keys[i] << std::endl;
+            test_check = false;
+            return 0;
+        }
+    }
 
-    std::cout << "ok." << std::endl;
+    std::cout << "serch_ok." << std::endl;
+
+    std::vector<std::string> keys_exp = {
+        "aaaaaaccc",
+        "aaaaaabbb",
+        "aaabbbccc",
+        "aaabbbbbb",
+        "bbbccccccddd",
+        "bbbcccccc",
+        "bbbbbbbbb",
+        "aaaaaaaaa"
+    };
+
+    std::cout << "比較するキー" << std::endl;
+    for(auto key : keys_exp) std::cout << key << std::endl;
 }
